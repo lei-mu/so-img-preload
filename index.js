a73d3cf37fa151d5d4bb755a9b7161bb36874d09
luch <webwork.s@qq.com>
2021-04-05 17:56:54 +0800

    refactor: 增加版本号信息

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+/**
+ * soImgPreload
+ * @version v1.0.1
+ * @author luch
+ * github: https://github.com/lei-mu/so-img-preload
+ * */
 import soImgPreload from './src/imgPreload'
 export default soImgPreload
 
